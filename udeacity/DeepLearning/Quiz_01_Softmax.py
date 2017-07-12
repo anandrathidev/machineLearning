ad2d9a2d07dbda845078c338f396796b4c48c672
AnandDev <anandrathi.dev@gmail.com>
2017-07-12 13:23:37 +0530

    Update Quiz_01_Softmax.py

--- udeacity/DeepLearning/Quiz_01_Softmax.py
+++ udeacity/DeepLearning/Quiz_01_Softmax.py
@@ -1,20 +1,43 @@
+
+## Your softmax(x) function should return a NumPy array of the same shape as x.
+## For example, given a list or one-dimensional array (which is interpreted as a column vector representing a single sample), like:
+
 import numpy as np
+def rowsums(myarray):
+    rsums = np.apply_along_axis( np.sum, axis=myarray.ndim-1, arr=myarray)
+    print(rsums)
+    print("rsums {}".format( rsums) )
+    if rsums.ndim>0:
+        rsums = rsums[:,np.newaxis]
+    print(rsums)
+    return rsums
+
+def colsums(myarray):
+    csums = np.apply_along_axis( np.sum, axis=0, arr=myarray)
+    print(csums)
+    print("rsums {}".format( csums) )
+    print(csums)
+    return csums
+
 
 def softmax(pscores):
     pscores = np.array(pscores)
-    escores = np.apply_along_axis( np.exp, axis=scores.ndim-1, arr=pscores )
-    print(escores)
-    rsums = np.apply_along_axis( np.sum, axis=escores.ndim-1, arr=escores )
-    print(rsums)
-    prvals = escores/ rsums
-    return prvals    
+    print("scores.ndim {}".format( pscores.ndim) )
+    escores = np.apply_along_axis( np.exp, axis=pscores.ndim-1, arr=pscores )
+    print("escores {}".format( escores) )
+    sums = colsums(myarray= escores)
+    print("sums {}".format( sums) )
+    prvals = escores/ sums
+    return prvals
 
 pscores = [1.0, 2.0, 3.0]
-pscores = np.array(pscores)
+#pscores = np.array(pscores)
 print( softmax(pscores ))
 
+#Given a 2-dimensional array where each column represents a sample,
 scores = np.array([[1, 2, 3, 6],
                    [2, 4, 5, 6],
                    [3, 8, 7, 6]])
 
-print( softmax(scores))
+res=  softmax(scores)
+print(res )
