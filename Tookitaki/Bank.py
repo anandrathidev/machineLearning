7e2c4998a48c518e73db100ecf54a4d5ea78aead
AnandDev <anandrathi.dev@gmail.com>
2017-08-27 00:46:13 +0530

    Update Bank.py

--- Tookitaki/Bank.py
+++ Tookitaki/Bank.py
@@ -47,6 +47,61 @@ view(Nullraw_account)
 for cols in  raw_account.columns[ pd.isnull(raw_account).sum() > 10 ]:
     raw_account[cols].describe()
 
+
+
+# Start Imputing 
+# =============================================================================
+#        high_credit_amt  amt_past_due  suitfiledorwilfuldefaultorwritte  \
+# count  1.774540e+05     8.760000e+02  0.0                                
+# mean   1.756104e+05     2.583151e+04 NaN                                 
+# std    9.842643e+05     2.030680e+05 NaN                                 
+# min    1.000000e+00     1.000000e+00 NaN                                 
+# 25%    2.297500e+04     1.535000e+02 NaN                                 
+# 50%    4.522350e+04     1.209500e+03 NaN                                 
+# 75%    1.060000e+05     7.663250e+03 NaN                                 
+# max    1.800000e+08     4.869309e+06 NaN                                 
+# 
+#        writtenoffandsettled  valueofcollateral   creditlimit       cashlimit  \
+# count  7195.000000           2.234000e+03       4.885200e+04  35282.000000     
+# mean   3.301598              2.112677e+06       7.552881e+04  20608.503401     
+# std    2.461591              3.655474e+06       7.174109e+04  24856.481410     
+# min    0.000000              6.000000e+03       1.000000e+00  1.000000         
+# 25%    3.000000              4.734630e+05       3.100000e+04  7500.000000      
+# 50%    3.000000              1.060815e+06       6.000000e+04  12500.000000     
+# 75%    4.000000              2.542350e+06       1.000000e+05  27000.000000     
+# max    99.000000             8.302000e+07       2.500000e+06  1000000.000000   
+# 
+#        emiamount  writtenoffamounttotal  writtenoffamountprincipal  \
+# count  0.0        1408.000000            1246.000000                 
+# mean  NaN         39417.474432           31259.667737                
+# std   NaN         63328.574121           56515.988520                
+# min   NaN         1.000000               1.000000                    
+# 25%   NaN         8149.500000            5149.750000                 
+# 50%   NaN         20622.500000           14244.000000                
+# 75%   NaN         43255.000000           32269.500000                
+# max   NaN         705013.000000          645547.000000               
+# 
+#        settlementamount  paymentfrequency  actualpaymentamount  \
+# count  8.560000e+02      63893.000000      4.105300e+04          
+# mean   2.736667e+04      2.996212          3.016616e+04          
+# std    5.466986e+04      0.086954          3.870822e+05          
+# min    4.320000e+02      1.000000          1.000000e+00          
+# 25%    7.037500e+03      3.000000          3.110000e+03          
+# 50%    1.500000e+04      3.000000          7.590000e+03          
+# 75%    2.929750e+04      3.000000          1.800000e+04          
+# max    1.126000e+06      3.000000          6.785322e+07          
+# 
+# =============================================================================
+# drop        disputeremarkscode2, dateofentryforerrorordisputerema, disputeremarkscode1
+#       dateofentryforerrorcode, errorcode, dateofentryforcibilremarkscode, emiamount
+#        suitfiledorwilfuldefaultorwritte, suitfiledorwilfuldefaultorwritte
+
+len(raw_account.columns    )
+raw_account = raw_account.drop(['disputeremarkscode2', 'dateofentryforerrorordisputerema', 'disputeremarkscode1',
+       'dateofentryforerrorcode', 'errorcode', 'dateofentryforcibilremarkscode', 'emiamount',
+        'suitfiledorwilfuldefaultorwritte', 'suitfiledorwilfuldefaultorwritte'], axis=1)    
+len(raw_account.columns    )
+    
 raw_account['opened_dt'].fillna( "01-01-1972", inplace=True)
 raw_account['opened_dt'].isnull().sum()
 
