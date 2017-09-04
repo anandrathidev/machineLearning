6bb395c706e98fc7fb50577d07e54be8368893bb
AnandDev <anandrathi.dev@gmail.com>
2017-09-04 08:10:08 +0530

    Update Bank.py

--- Tookitaki/Bank.py
+++ Tookitaki/Bank.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Mon Aug 28 11:06:35 2017
 
+@author: rb117
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
 This is a temporary script file.
 """
 
@@ -15,16 +21,29 @@ pd.set_option('display.max_columns', None) # Display any number of columns
 pd.set_option('display.max_rows', 100) # Display any number of rows
 pd.set_option('display.max_colwidth', -1)
 
-raw_account = pd.read_csv('D:/Users/anandrathi/Documents/Work/InterView/RBL_70_30/RBL_70_30/raw_account_70.csv')
-raw_data = pd.read_csv('D:/Users/anandrathi/Documents/Work/InterView/RBL_70_30/RBL_70_30/raw_data_70.csv')
-raw_enquiry = pd.read_csv('D:/Users/anandrathi/Documents/Work/InterView/RBL_70_30/RBL_70_30/raw_enquiry_70.csv', low_memory=False)
+def rstr(df, toponly=True):
+  if toponly==True:
+    return df.shape, df.apply(lambda x:  ("NA Count= {}".format(sum(x.isnull()))
+                                          ,  [x.unique()[1:10] ]) )
+  else:
+    return df.shape, df.apply(lambda x: ( sum(x.isnull()) ,  [x.unique()]) )
 
-raw_account.describe(include = 'all')
-raw_data.describe() 
-raw_enquiry.describe()
-raw_account.columns[raw_account.isnull().any()].tolist()
+base_path = "C:/Users/rb117/Documents/personal/Tookitaki/RBL_70_30/RBL_70_30/"
+#base_path = "D:/Users/anandrathi/Documents/Work/InterView/RBL_70_30/RBL_70_30/"
+raw_account_init = pd.read_csv(base_path + 'raw_account_70.csv')
+raw_data_init = pd.read_csv(base_path + 'raw_data_70.csv')
+raw_enquiry_init = pd.read_csv(base_path + 'raw_enquiry_70.csv', low_memory=False)
+
+#raw_account.describe(include = 'all')
+#raw_data.describe() 
+#raw_enquiry.describe()
+#raw_account.columns[raw_account.isnull().any()].tolist()
+
+raw_account = raw_account_init.copy(deep=True) 
 
 #view(raw_account)
+########################   raw_account ########################
+################################################################   
 
 row_ids = raw_account[raw_account["opened_dt"] != raw_account["dt_opened"]].index
 erow_ids = raw_account[raw_account["opened_dt"] == raw_account["dt_opened"]].index
@@ -42,8 +61,8 @@ Nullraw_account.describe()
 Nullraw_account.columns
 #view(Nullraw_account)
 
-for cols in  raw_account.columns[ pd.isnull(raw_account).sum() > 10 ]:
-    raw_account[cols].describe()
+#for cols in  raw_account.columns[ pd.isnull(raw_account).sum() > 10 ]:
+#  print(raw_account[cols].describe())
 
 
 
@@ -100,14 +119,39 @@ raw_account = raw_account.drop(['disputeremarkscode2', 'dateofentryforerrorordis
         'suitfiledorwilfuldefaultorwritte', 'suitfiledorwilfuldefaultorwritte'], axis=1)    
 len(raw_account.columns    )
     
-raw_account['opened_dt'].fillna( "01-01-1972", inplace=True)
-raw_account['opened_dt'].isnull().sum()
 
-raw_account[['opened_dt']] =  raw_account['opened_dt'].apply( lambda x :  x[0:6] + "-20" + x[:2])
-raw_account['opened_dt'][raw_account['opened_dt'].str.len() > 11]
-raw_account['opened_dt'].head()
-raw_account['opened_dt'].describe()
-pd.to_datetime(raw_account['opened_dt'], format='%Y%m%d', errors='ignore')
+raw_account['upload_dt'] =  pd.to_datetime(raw_account['upload_dt'][raw_account['upload_dt'].notnull()].apply( lambda x :  x[0:6] + "-20" + x[-2:]))
+raw_account['upload_dt'].dtype
+sum(raw_account['upload_dt'].isnull())
+
+raw_account['last_paymt_dt'] =  pd.to_datetime(raw_account['last_paymt_dt'][raw_account['last_paymt_dt'].notnull()].apply( lambda x :  x[0:6] + "-20" + x[-2:]))
+raw_account['last_paymt_dt'].fillna(raw_account['upload_dt'] , inplace=True)
+
+raw_account['closed_dt'] =  pd.to_datetime(raw_account['closed_dt'][raw_account['closed_dt'].notnull()].apply( lambda x :  x[0:6] + "-20" + x[-2:]))
+raw_account['closed_dt'].fillna( pd.to_datetime('today'), inplace=True)
+raw_account['closed_dt'].dtype
+
+raw_account['reporting_dt'] =  pd.to_datetime(raw_account['reporting_dt'][raw_account['reporting_dt'].notnull()].apply( lambda x :  x[0:6] + "-20" + x[-2:]))
+raw_account['reporting_dt'].dtype
+
+raw_account['paymt_str_dt'] =  pd.to_datetime(raw_account['paymt_str_dt'][raw_account['paymt_str_dt'].notnull()].apply( lambda x :  x[0:6] + "-20" + x[-2:]))
+raw_account['paymt_str_dt'].dtype
+raw_account['paymt_end_dt'] =  pd.to_datetime(raw_account['paymt_end_dt'][raw_account['paymt_end_dt'].notnull()].apply( lambda x :  x[0:6] + "-20" + x[-2:]))
+raw_account['paymt_end_dt'].dtype
+raw_account['dt_opened']    =  pd.to_datetime(raw_account['dt_opened'][raw_account['dt_opened'].notnull()].apply( lambda x :  x[0:6] + "-20" + x[-2:]))
+raw_account['dt_opened'].dtype
+
+pd.to_datetime('today')
+
+raw_account['opened_dt'].dtype
+raw_account['opened_dt'] =  raw_account['opened_dt'][raw_account['opened_dt'].notnull()].apply( lambda x :  x[0:6] + "-20" + x[-2:])
+raw_account['opened_dt'].fillna(raw_account['dt_opened'] , inplace=True)
+
+raw_account['opened_dt'].fillna( pd.to_datetime("01-01-1970"), inplace=True)
+raw_account['opened_dt'].isnull().sum()
+raw_account['opened_dt'] = pd.to_datetime(raw_account['opened_dt'])
+raw_account["dt_opened"] - raw_account["opened_dt"] 
+
 
 sum(raw_account["typeofcollateral"].isnull() )
 len(raw_account["typeofcollateral"][ raw_account["typeofcollateral"].isnull() == False])
@@ -118,13 +162,12 @@ raw_account['typeofcollateral'].unique()
 raw_account["typeofcollateral"]  = raw_account["typeofcollateral"].astype('category')
 raw_account["typeofcollateral"].describe()
 # Impute 
-raw_account["typeofcollateral"] 
+#raw_account["typeofcollateral"] 
 
 sum(raw_account["high_credit_amt"].isnull() )
 sum(raw_account["high_credit_amt"]==0 )
 raw_account["high_credit_amt"] 
 raw_account['high_credit_amt'].fillna( 0, inplace=True)
-
 
 sum(raw_account["amt_past_due"].isnull() )
 sum(raw_account["amt_past_due"]==0 )
@@ -136,7 +179,6 @@ sum(raw_account["writtenoffandsettled"].isnull() )
 sum(raw_account["writtenoffandsettled"]==0 )
 sum(raw_account["writtenoffandsettled"]>0 )
 raw_account['writtenoffandsettled'].fillna( 0, inplace=True)
-
 
 sum(raw_account["writtenoffamountprincipal"].isnull() )
 sum(raw_account["writtenoffamountprincipal"]==0 )
@@ -169,5 +211,88 @@ sum(raw_account["writtenoffamounttotal"]>0 )
 raw_account['writtenoffamounttotal'].fillna( 0, inplace=True)
 
 
+sum(raw_account["paymentfrequency"].isnull())
+sum(raw_account["paymentfrequency"]==0 )
+sum(raw_account["paymentfrequency"]>0 )
+raw_account['paymentfrequency'].fillna( 0, inplace=True)
+
+raw_account['rateofinterest']  = pd.to_numeric(raw_account['rateofinterest'], errors='coerce')
+sum(raw_account["rateofinterest"].isnull())
+sum(raw_account["rateofinterest"]==0 )
+sum(raw_account["rateofinterest"]>0 )
+raw_account['rateofinterest'].fillna( 0, inplace=True)
+sum(raw_account["rateofinterest"].isnull())
 
 
+raw_account['repaymenttenure']  = pd.to_numeric(raw_account['repaymenttenure'], errors='coerce')
+sum(raw_account["repaymenttenure"].isnull())
+sum(raw_account["repaymenttenure"]==0 )
+sum(raw_account["repaymenttenure"]>0 )
+raw_account['repaymenttenure'].fillna( 0, inplace=True)
+sum(raw_account["repaymenttenure"].isnull())
+
+raw_account['settlementamount']  = pd.to_numeric(raw_account['repaymenttenure'], errors='coerce')
+sum(raw_account["settlementamount"].isnull())
+sum(raw_account["settlementamount"]==0 )
+sum(raw_account["settlementamount"]>0 )
+raw_account['settlementamount'].fillna( 0, inplace=True)
+
+
+sum(raw_account["actualpaymentamount"].isnull())
+raw_account['actualpaymentamount']  = pd.to_numeric(raw_account['actualpaymentamount'], errors='coerce')
+raw_account['actualpaymentamount'].fillna( 0, inplace=True)
+sum(raw_account["actualpaymentamount"].isnull())
+
+sum(raw_account["cibilremarkscode"].isnull())
+raw_account['cibilremarkscode'].fillna( "NotAvailaible", inplace=True)
+raw_account["cibilremarkscode"] = raw_account["cibilremarkscode"].astype('category')
+raw_account["cibilremarkscode"].describe()
+
+raw_account.isnull().sum()
+
+sum(raw_account["cibilremarkscode"].isnull())
+
+raw_account[raw_account['paymt_str_dt'].isnull() | raw_account['paymt_end_dt'].isnull() ]
+raw_account['paymt_str_dt'][raw_account['paymt_str_dt'].isnull()]  = raw_account['opened_dt'][raw_account['paymt_str_dt'].isnull()] 
+raw_account['paymt_end_dt'][raw_account['paymt_end_dt'].isnull()]  = raw_account['opened_dt'][raw_account['paymt_end_dt'].isnull()] 
+
+
+sum(raw_account["id"] == raw_account["externalid"] )
+raw_account["id"].head()
+raw_account["member_short_name"].head()
+
+raw_account = raw_account.drop(['id', 'externalid'], axis=1)
+raw_account = raw_account.drop(['paymenthistory1', 'paymenthistory2'], axis=1)
+
+raw_account['paymenthistory1']  = raw_account_init['paymenthistory1'].str.replace('"""', '').str.slice(start=0, stop=3, step=None)
+phu = raw_account['paymenthistory1'].unique()
+
+rstr(raw_account )
+pd.isnull(raw_account).sum()
+
+rstr(raw_data)
+raw_account.columns
+raw_data.columns
+
+raw_data.shape
+raw_account.shape
+
+pd.isnull(raw_data).sum()
+
+
+df = pd.DataFrame({
+   'x': np.random.uniform(1., 168., 12),
+   'y': np.random.uniform(7., 334., 12),
+   'z': np.random.uniform(1.7, 20.7, 12),
+   'month': [5,6,7]*4,
+   'week': [1,2]*6
+   })
+    
+df = pd.DataFrame({
+   'Animal': ['Animal1', 'Animal2', 'Animal3', 'Animal2', 'Animal1',
+   'Animal2', 'Animal3'],
+   'FeedType': ['A', 'B', 'A', 'A', 'B', 'B', 'A'],
+   'Amount': [10, 7, 4, 2, 5, 6, 2],
+   })    
+
+ndf = df.pivot_table(values='Amount', index='Animal', columns='FeedType', aggfunc='sum')    
