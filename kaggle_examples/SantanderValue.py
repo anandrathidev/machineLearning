c21200c6bb05e396000d1ba872de7909d734211c
AnandDev <anandrathi.dev@gmail.com>
2018-06-25 17:04:49 +0800

    Add files via upload

--- kaggle_examples/SantanderValue.py
+++ kaggle_examples/SantanderValue.py
@@ -22,33 +22,44 @@ np.random.seed(1)
 
 import pandas as pd
 
-data_file = "D:/Users/anandrathi/Documents/Work/Kaggle/Santander/train.csv/train.csv"
-y_RF_file = "D:/Users/anandrathi/Documents/Work/Kaggle/Santander//RF_y.csv"
+filepath = "F:/DataScience/Kagggle/SantanderValue/"
+data_file = filepath + "train.csv"
+y_RF_file = filepath + "/tmp/RF_y.csv"
+finalyrbf_file = filepath + "/tmp///RFrbf_y.csv"
+yXGS_file = filepath + "/tmp/XGS_y.csv"
 
-data = None
-Y = None
-try:
-  data = pd.read_csv(data_file)
-  list(data.columns)
-  Y = data["target"]
-  data = data.drop(columns=['ID', 'target'])
-except Exception as e:
-  print(e)
+test_file = filepath + "test.csv"
+test_result_file1 = filepath + "/tmp/ypredDFFinalDetails.csv"
+test_result_file2 = filepath + "/tmp//FinalTestResult.csv"
 
+test_result_file21 = filepath + "/tmp//FinalTestResultarget.csv"
+test_result_file22 = filepath + "/tmp//FinalTestResultRetarget.csv"
 
-print(list(data.columns))
-#print(list(Y.columns))
+PrepareData=True
+if PrepareData:
+    data = None
+    Y = None
+    try:
+      data = pd.read_csv(data_file)
+      list(data.columns)
+      Y = data["target"]
+      data = data.drop(columns=['ID', 'target'])
+    except Exception as e:
+      print(e)
+    
+    Y = np.log1p(Y)
+    print(list(data.columns)[0:10])
+    #print(list(Y.columns))
 
 #=============================================================================
 # start Feature selection
 #=============================================================================
 
-
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import f_regression
-from  sklearn.feature_selection import mutual_info_regression
-from  sklearn.preprocessing import RobustScaler
+from sklearn.feature_selection import mutual_info_regression
+from sklearn.preprocessing import RobustScaler
 from sklearn import preprocessing
 from sklearn.decomposition import SparsePCA
 
@@ -60,14 +71,23 @@ rscaler = RobustScaler()
 rscaler.fit(data)
 data = rscaler.transform(data)
 
-pca = decomposition.PCA(0.99)
+pca = decomposition.PCA(0.94)
 pca.fit(data)
-X = pca.transform(data)
-X.shape
+data = pca.transform(data)
+data.shape
 
 #X_normalized = preprocessing.normalize(data, norm='l2')
 
-data=X
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+lreg = LinearRegression()
+lreg.fit(data,Y)
+y_Lpred = lreg.predict(data)
+
+trainrms = sqrt(mean_squared_error(Y, y_Lpred))
+print("LREG : trainrms {}".format(trainrms ) )
 
 #=============================================================================
 # end Feature selection
@@ -84,7 +104,6 @@ RFregr.fit(data, Y)
 # Make the prediction on the meshed x-axis (ask for MSE as well)
 y_pred = RFregr.predict(data)
 
-
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 
@@ -95,7 +114,7 @@ ypredDF = pd.DataFrame(y_pred , columns=["YHat"])
 ypredDF.shape
 
 trainrms = sqrt(mean_squared_error(Y, ypredDF['YHat']))
-print("RF : trainrms {}".format(trainrms ) )
+print("RFPCA : trainrms {}".format(trainrms ) )
 #=============================================================================
 # end RF
 #=============================================================================
@@ -104,8 +123,8 @@ print("RF : trainrms {}".format(trainrms ) )
 #=============================================================================
 
 import xgboost
-xgb = xgboost.XGBRegressor(n_estimators=100, learning_rate=0.08, gamma=0, subsample=0.75,
-                           colsample_bytree=1, max_depth=7)
+xgb = xgboost.XGBRegressor(n_estimators=101, learning_rate=0.59, gamma=5, subsample=0.81,
+                           colsample_bytree=1, max_depth=15)
 
 # Fit to data using Maximum Likelihood Estimation of the parameters
 xgb.fit(data, Y)
@@ -122,9 +141,8 @@ ypredDFXGS = pd.DataFrame(y_xgbpred, columns=["YHatXGS"])
 ypredDFXGS["Y"] = Y
 
 trainrms = sqrt(mean_squared_error(Y, y_xgbpred))
-print("XGS : trainrms {}".format(trainrms ) )
+print("XGSPCA : trainrms {}".format(trainrms ) )
 
-yXGS_file = "D:/Users/anandrathi/Documents/Work/Kaggle/Santander//XGS_y.csv"
 ypredDFXGS.to_csv(yXGS_file)
 
 #=============================================================================
@@ -133,8 +151,8 @@ ypredDFXGS.to_csv(yXGS_file)
 
 
 # Instanciate a Gaussian Process model
-kernel = C(5.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
+kernel = C(7.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+gp = GaussianProcessRegressor(kernel=None, alpha=0.0001,  n_restarts_optimizer=33, normalize_y=True)
 ygp = gp.fit(data, Y)
 
 
@@ -146,18 +164,17 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 #y_pred = np.around(y_pred, decimals=2)
-ypredDFFinal = pd.DataFrame( {y_RBFpred, y_xgbpred, y_pred , Y}  , columns=["RBF", "xgb", "RF", "Y"])
 
 ypredDFFinal =  pd.DataFrame(dict(RBF = y_RBFpred, XGS= y_xgbpred, RF = y_pred , Y=Y))
 
 trainrms = sqrt(mean_squared_error(Y, y_RBFpred))
 print("RBFPCA : trainrms {}".format(trainrms ) )
 
-finalyrbf_file = "D:/Users/anandrathi/Documents/Work/Kaggle/Santander//RFrbf_y.csv"
 
 ypredDFFinal.to_csv(finalyrbf_file)
 
 ypredDFFinal =  pd.DataFrame(dict(RBF = y_RBFpred, XGS= y_xgbpred, RF = y_pred ))
+ypredDFFinal.shape
 
 
 #=============================================================================
@@ -165,42 +182,74 @@ ypredDFFinal =  pd.DataFrame(dict(RBF = y_RBFpred, XGS= y_xgbpred, RF = y_pred )
 #=============================================================================
 
 
+xgbFinal = xgboost.XGBRegressor(n_estimators=101, learning_rate=0.19, gamma=130, subsample=0.71,
+                           colsample_bytree=1, max_depth=15)
+xgbFinal.fit(ypredDFFinal,Y)
+
+YxgbFinal = xgbFinal.predict(ypredDFFinal)
+trainrms = sqrt(mean_squared_error(Y, YxgbFinal))
+print("RBFFinal : trainrms {}".format(trainrms ) )
+
+
 #=============================================================================
 # start TEST
 #=============================================================================
 
+if PrepareData:
+    testdata = None
+    Ytest = None
+    testID = None
+    try:
+      testdata = pd.read_csv(test_file)
+      print("Test Data Loaded...")
+      #list(testdata.columns)
+      testID = testdata["ID"]
+      testdata = testdata.drop(columns=['ID'])
+      testdata = rscaler.transform(testdata)
+      print("Test Data scaled...")
+      testdata = pca.transform(testdata)
+      print("Test Data pca...")
+    except Exception as e:
+      print(e)
 
-test_file = "D:/Users/anandrathi/Documents/Work/Kaggle/Santander/test.csv"
-testdata = None
-Ytest = None
-testID = None
-try:
-  testdata = pd.read_csv(test_file)
-  list(testdata.columns)
-  testID = testdata["ID"]
-  testdata = testdata.drop(columns=['ID'])
-  testdata = pca.transform(testdata)
-except Exception as e:
-  print(e)
-
+print("Predict  RF...")
 ytest_RF = RFregr.predict(testdata)
+print("Predict  xgb...")
 ytest_XGS = xgb.predict(testdata)
+print("Predict  rbf...")
 ytest_RBF = gp.predict(testdata)
 
+ypredDFFinalDetails = pd.DataFrame(dict(RBF = ytest_RBF, XGS= ytest_XGS, RF = ytest_RF, Target= (1*ytest_RBF + 100*ytest_XGS + ytest_RF)/102  ))
+ypredDFFinalDetails.to_csv(test_result_file1, index=False)
 ypredDFFinalDetails = pd.DataFrame(dict(RBF = ytest_RBF, XGS= ytest_XGS, RF = ytest_RF))
-test_result_file = "D:/Users/anandrathi/Documents/Work/Kaggle/Santander/ypredDFFinalDetails.csv"
-ypredDFFinalDetails.to_csv(test_result_file, index=False)
+ypredDFFinalDetails.shape
 
-gpFinal = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
-gpFinal.fit(ypredDFFinal, Y)
-ygpTestFinal = gpFinal.predict(ypredDFFinalDetails)
+print("Predict  xgs final ...")
+ygpTestFinal = xgbFinal.predict(ypredDFFinalDetails)
 
-ytestpredDF = pd.DataFrame(testID, columns=["ID"])
-ytestpredDF["target"] = (ytest_RF + ytest_XGS + 3*ygpTestFinal)/5
-test_result_file = "D:/Users/anandrathi/Documents/Work/Kaggle/Santander/FinalTestResult.csv"
-ytestpredDF.to_csv(test_result_file, index=False)
+print("Write test data ...")
+ypredDFFinalDetails["ID"] = testID
+ypredDFFinalDetails["YGP"] = ygpTestFinal
 
-ytestpredDF.shape
+ypredDFFinalDetails["ReTarget"] = np.rint(np.exp( (ytest_RF + ytest_XGS + 3*ygpTestFinal)/5 +1))
+ypredDFFinalDetails["Target"] =  np.rint(np.exp((3*ytest_RBF + 10*ytest_XGS + ytest_RF)/14  +1))
+
+ypredDFFinalDetails.to_csv(test_result_file2, index=False)
+
+sumbission1 = ypredDFFinalDetails[["ID"]]
+sumbission1["target"] = ypredDFFinalDetails["Target"] 
+sumbission1.to_csv(test_result_file21, index=False)
+
+sumbission2 = ypredDFFinalDetails[["ID"]]
+sumbission2["target"] = ypredDFFinalDetails["ReTarget"] 
+sumbission2.to_csv(test_result_file22, index=False)
+
+
+sumbission2 = ypredDFFinalDetails[["ID"]]
+sumbission2["target"] = np.rint(np.exp( ytest_XGS -1))
+sumbission2.to_csv(filepath + "/tmp/XGSTarget.csv" , index=False)
+
+ypredDFFinalDetails.shape
 
 #=============================================================================
 # start TEST RBF
