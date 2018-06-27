5bb1a0f0977b6d4a4270fd124c7b9f5cc5d4198f
AnandDev <anandrathi.dev@gmail.com>
2018-06-27 18:11:09 +0800

    Add files via upload

--- kaggle_examples/SantanderValueLog.py
+++ kaggle_examples/SantanderValueLog.py
@@ -16,7 +16,21 @@ from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectPercentile
+from sklearn.feature_selection import f_regression
+from sklearn.feature_selection import mutual_info_regression
+from sklearn.preprocessing import RobustScaler
+from sklearn import preprocessing
+from sklearn.decomposition import SparsePCA
+from sklearn import feature_selection
 
+from sklearn import decomposition
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import numpy as np
 
 np.random.seed(1)
 
@@ -51,6 +65,18 @@ if PrepareData:
     Y = Y
     print(list(data.columns)[0:10])
     #print(list(Y.columns))
+    testdata = None
+    Ytest = None
+    testID = None
+    try:
+      testdata = pd.read_csv(test_file)
+      print("Test Data Loaded...")
+      #list(testdata.columns)
+      testID = testdata["ID"]
+          
+    except Exception as e:
+      print(e)
+
 
 from scipy.stats import boxcox
 
@@ -65,43 +91,59 @@ def invboxcox(y,ld):
 Yt = invboxcox(YBC,ylambda)
 Ydf = pd.DataFrame(dict(Y=Y,Yt=Yt))
 
+plt.figure(figsize=(8,8))
+plt.plot(range(0,len(Y)),np.sort(Ydf["Y"]))
+plt.show()
+
 Y=YBC
 #=============================================================================
 # start Feature selection
 #=============================================================================
 
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.feature_selection import SelectPercentile
-from sklearn.feature_selection import f_regression
-from sklearn.feature_selection import mutual_info_regression
-from sklearn.preprocessing import RobustScaler
-from sklearn import preprocessing
-from sklearn.decomposition import SparsePCA
 
-from sklearn import decomposition
-from sklearn import datasets
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
+plt.plot(range(0,len(Y)),np.sort(Y))
+plt.show()
 
-plt.plot(Y,Y)
-from sklearn import feature_selection
 #from sklearn.preprocessing import PowerTransformer
 
 print("Feature selection...")
 #FSelect = feature_selection.SelectPercentile(f_regression, percentile=30)
 #FSelect.fit(data, Y)
 #data = FSelect.transform(data)
+
+testdata = testdata.drop(columns=['ID'])
+
+fulldata = data.append(testdata)
+colsToRemove = []
+for col in fulldata.columns:
+  if col != 'ID' and col != 'target':
+    if fulldata[col].std() == 0: 
+      colsToRemove.append(col)
+        
+len(colsToRemove) 
+
+# remove constant columns in the test set
+testdata.drop(colsToRemove, axis=1, inplace=True)       
+#testdata= FSelect.transform(testdata)
+print("Test Data scaled...")
+
+data.drop(colsToRemove, axis=1, inplace=True)
+
 print("scale...")
 rscaler = RobustScaler()
-rscaler.fit(data)
-data = rscaler.transform(data)
+rscaler.fit(fulldata)
+print("train pca...")
+pca = decomposition.PCA(0.995)
+pca.fit(fulldata)
 
-pca = None
-pca = decomposition.PCA(0.90)
-print("PCA...")
-pca.fit(data)
+print("PCA data...")
+data = rscaler.transform(data)
 data = pca.transform(data)
+
+print("Test Data pca...")
+testdata = rscaler.transform(testdata)
+testdata = pca.transform(testdata)
+
 #outliers = stats.zscore(data['_source.price']).apply(lambda x: np.abs(x) == 3)
 #df_without_outliers = data[~outliers]
 
@@ -115,11 +157,14 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 #print("Lin reg...")
-#lreg = LinearRegression()
-#lreg.fit(data,Y)
-#y_Lpred = lreg.predict(data)
-#trainrms = sqrt(mean_squared_error(Y, y_Lpred))
-#print("LREG : trainrms {}".format(trainrms ) )
+lreg = LinearRegression()
+lreg.fit(data,Y)
+y_Lpred = lreg.predict(data)
+trainrms = sqrt(mean_squared_error(Y, y_Lpred))
+print("LREG : trainrms {}".format(trainrms ) )
+plt.figure(figsize=(8,8))
+plt.scatter( Y, y_Lpred)
+plt.show()
 
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
@@ -219,20 +264,26 @@ from math import sqrt
 
 #y_pred = np.around(y_pred, decimals=2)
 
-ypredDFFinal =  pd.DataFrame(dict(RBF = y_RBFpred, XGS= y_xgbpred, RF = y_pred , Y=Y))
+ypredDFFinal =  pd.DataFrame(dict(KNN = knnreg, RBF = y_RBFpred, XGS= y_xgbpred, RF = y_pred, Y=Y))
 
 trainrms = sqrt(mean_squared_error(Y, y_RBFpred))
 print("RBFPCA : trainrms {}".format(trainrms ) )
-
 
 ypredDFFinal.to_csv(finalyrbf_file)
 
 
 plt.figure(figsize=(8,8))
 plt.scatter( Y, y_pred )
+plt.show()
+plt.figure(figsize=(8,8))
 plt.scatter( Y, y_RBFpred )
+plt.show()
+plt.figure(figsize=(8,8))
 plt.scatter( Y, y_xgbpred )
+plt.show()
+plt.figure(figsize=(8,8))
 plt.scatter( Y, y_KNNpred )
+plt.show()
 
 
 plt.xlabel('index', fontsize=12)
@@ -245,7 +296,7 @@ plt.show()
 #=============================================================================
 
 print("xgbFinal ...{}".format("" ) )
-ypredDFFinal =  pd.DataFrame(dict(XGS= y_xgbpred, RF = y_pred , KNN=y_KNNpred))
+ypredDFFinal =  pd.DataFrame(dict( KNN=y_KNNpred, RBF = y_RBFpred, XGS= y_xgbpred, RF = y_pred))
 ypredDFFinal.shape
 
 
@@ -253,11 +304,53 @@ ypredDFFinal.shape
 xgbFinal = xgboost.XGBRegressor(n_estimators=201, learning_rate=0.19, gamma=130, subsample=0.71,
                            colsample_bytree=1, max_depth=15)
 xgbFinal.fit(ypredDFFinal,Y)
-
 YxgbFinal = xgbFinal.predict(ypredDFFinal)
 trainrms = sqrt(mean_squared_error(Y, YxgbFinal))
-
 print("XGSFinal : trainrms {}".format(trainrms ) )
+plt.figure(figsize=(11,11))
+plt.scatter(Y, YxgbFinal, s=100,  marker="s", label='YxgbFinal')
+plt.xlabel('index', fontsize=12)
+plt.ylabel('YxgbFinal ', fontsize=12)
+plt.title("Y", fontsize=14)
+plt.show()
+
+gpFinal = GaussianProcessRegressor(kernel=None, alpha=0.0001,  n_restarts_optimizer=33, normalize_y=True)
+gpFinal.fit(ypredDFFinal, Y)
+YGPFinal = gpFinal.predict(ypredDFFinal)
+trainrms = sqrt(mean_squared_error(Y, YGPFinal))
+print("YGPinal : trainrms {}".format(trainrms ) )
+plt.figure(figsize=(11,11))
+plt.scatter(Y, YGPFinal, s=100,  marker="s", label='YGPFinal')
+plt.xlabel('index', fontsize=12)
+plt.ylabel('YGPFinal ', fontsize=12)
+plt.title("Y", fontsize=14)
+plt.show()
+
+RFregrFinal = RandomForestRegressor(n_estimators=311, random_state=0, oob_score=True)
+# Fit to data using Maximum Likelihood Estimation of the parameters
+RFregrFinal.fit(ypredDFFinal, Y)
+YRFFinal = RFregrFinal.predict(ypredDFFinal)
+trainrms = sqrt(mean_squared_error(Y, YRFFinal))
+print("YRFFinal : trainrms {}".format(trainrms ) )
+plt.figure(figsize=(11,11))
+plt.scatter(Y, YRFFinal, s=100,  marker="s", label='YRFFinal')
+plt.xlabel('index', fontsize=12)
+plt.ylabel('YRFFinal ', fontsize=12)
+plt.title("Y", fontsize=14)
+plt.show()
+
+lregFinal = LinearRegression()
+lregFinal.fit(data,Y)
+YLFinal  = lregFinal.predict(data)
+trainrms = sqrt(mean_squared_error(Y, YLFinal ))
+print("YRFFinal : trainrms {}".format(trainrms ) )
+
+plt.figure(figsize=(11,11))
+plt.scatter(Y, YLFinal, s=100,  marker="s", label='LinFinal')
+plt.xlabel('index', fontsize=12)
+plt.ylabel('YLFinal', fontsize=12)
+plt.title("Y", fontsize=14)
+plt.show()
 
 #radregF  = RadiusNeighborsRegressor(weights='distance', radius=1.3)
 #radregF.fit(ypredDFFinal,Y) 
@@ -266,26 +359,6 @@ print("XGSFinal : trainrms {}".format(trainrms ) )
 #=============================================================================
 # start TEST
 #=============================================================================
-
-if PrepareData:
-    testdata = None
-    Ytest = None
-    testID = None
-    try:
-      testdata = pd.read_csv(test_file)
-      print("Test Data Loaded...")
-      #list(testdata.columns)
-      testID = testdata["ID"]
-      testdata = testdata.drop(columns=['ID'])
-      #testdata= FSelect.transform(testdata)
-      print("Test Data scaled...")
-      testdata = rscaler.transform(testdata)
-      print("Test Data pca...")
-      if pca is not None:
-          testdata = pca.transform(testdata)
-          
-    except Exception as e:
-      print(e)
 
 print("Predict  RF...")
 ytest_RF = invboxcox(RFregr.predict(testdata),ylambda)
@@ -327,59 +400,70 @@ plt.ylabel('ytest_RF', fontsize=12)
 plt.title("ytest_RF Distribution", fontsize=14)
 plt.show()
 
-plt.figure(figsize=(11,11))
-plt.scatter(range(0,len(ytest_XGS)), ygpTestFinal, s=100,  marker="s", label='ygpTestFinal')
-plt.xlabel('index', fontsize=12)
-plt.ylabel('ytest_RF', fontsize=12)
-plt.title("ygpTestFinal Distribution", fontsize=14)
-plt.scatter(range(0,len(ytest_XGS)), ygpTestFinal, s=100,  marker="s", label='ygpTestFinal, ytest_KNNpred ')
-plt.show()
 
 plt.figure(figsize=(11,11))
-plt.scatter(range(0,len(ytest_XGS)), ygpTestFinal, s=100,  marker="s", label='ygpTestFinal')
-plt.xlabel('index', fontsize=12)
-plt.ylabel('ytest_RF', fontsize=12)
-plt.title("Target Distribution", fontsize=14)
-plt.scatter(range(0,len(ytest_XGS)), (ytest_RBF + ytest_KNNpred  + 2*ygpTestFinal)/4, s=100,  marker="s", label='ygpTestFinal, ytest_KNNpred ')
-plt.show()
-
-
 plt.scatter(ytest_RF, ytest_XGS, s=100,  marker="s", label='RF-XGS')
 plt.scatter(ytest_RF, ytest_RBF, s=100,  marker="s", label='RF-RBF')
 plt.scatter(ytest_XGS, ytest_RBF, s=100,  marker="s", label='XGS-RBF')
 plt.scatter(ytest_XGS, ytest_KNNpred, s=100,  marker="s", label='XGS-KNN')
 plt.scatter(ytest_RBF, ytest_KNNpred, s=100,  marker="s", label='ytest_RBF, ytest_KNNpred ')
 plt.scatter(ytest_RF, ytest_KNNpred, s=100,  marker="s", label='ytest_RF, ytest_KNNpred ')
-
-plt.scatter(ygpTestFinal, ytest_XGS, s=100,  marker="s", label='ygpTestFinal, ytest_KNNpred ')
-plt.scatter(ygpTestFinal, ytest_RF, s=100,  marker="s", label='ygpTestFinal, ytest_KNNpred ')
-
 plt.xlabel('index', fontsize=12)
 plt.ylabel('Target', fontsize=12)
-plt.title("Target Distribution", fontsize=14)
+plt.title("Distribution", fontsize=14)
 plt.show()
+
 
 
 ypredDFFinalDetails = pd.DataFrame(dict(XGS= ytest_XGS, RF = ytest_RF, KNN=ytest_KNNpred, RBF=ytest_RBF, Target= ( 2*ytest_XGS + ytest_RF + 2*ytest_KNNpred)/5  ))
 ypredDFFinalDetails.to_csv(test_result_file1, index=False)
-ypredDFFinalDetails = pd.DataFrame(dict( XGS=ytest_XGS, RF = ytest_RF, KNN = ytest_KNNpred))
+ypredDFFinalDetails = pd.DataFrame(dict( KNN=ytest_KNNpred, RBF = ytest_RBF, XGS= ytest_XGS, RF = ytest_RF  ))
+
 ypredDFFinalDetails.shape
 
 print("Predict  xgs final ...")
-ygpTestFinal = invboxcox(xgbFinal.predict(ypredDFFinalDetails), ylambda)
+yxgsTestFinal = invboxcox(xgbFinal.predict(ypredDFFinalDetails), ylambda)
+ygpTestFinal = invboxcox(gpFinal.predict(ypredDFFinalDetails), ylambda) 
+yRFTestFinal = invboxcox(RFregrFinal.predict(ypredDFFinalDetails), ylambda) 
+
+
 #yradTestFinal = radregF.predict(ypredDFFinalDetails)
+plt.figure(figsize=(11,11))
+plt.scatter(range(0,len(yxgsTestFinal)), yxgsTestFinal, s=100,  marker="s", label='yxgsTestFinal')
+plt.xlabel('index', fontsize=12)
+plt.ylabel('yxgsTestFinal ', fontsize=12)
+plt.title("yxgsTestFinal Distribution", fontsize=14)
+plt.show()
+
+plt.figure(figsize=(11,11))
+plt.scatter(range(0,len(ygpTestFinal)), ygpTestFinal, s=100,  marker="s", label='ygpTestFinal')
+plt.xlabel('index', fontsize=12)
+plt.ylabel('ygpTestFinal ', fontsize=12)
+plt.title("ygpTestFinal Distribution", fontsize=14)
+plt.show()
+
+plt.figure(figsize=(11,11))
+plt.scatter(range(0,len(yRFTestFinal)), yRFTestFinal, s=100,  marker="s", label='yRFTestFinal')
+plt.xlabel('index', fontsize=12)
+plt.ylabel('yRFTestFinal ', fontsize=12)
+plt.title("yRFTestFinal Distribution", fontsize=14)
+plt.show()
+
+
+ypredFinalFinal = pd.DataFrame(dict(FXGS=yxgsTestFinal, FRF=yRFTestFinal, FGP= ygpTestFinal ))
+ypredFinalFinal.to_csv(filepath + "/tmp/FinalFinal.csv", index=False)
 
 print("Write test data ...")
 ypredDFFinalDetails["ID"] = testID
 ypredDFFinalDetails["YGP"] = ygpTestFinal
 
 ypredDFFinalDetails["ReTarget"] = (ytest_RBF + ytest_KNNpred + ytest_XGS + 2*ygpTestFinal)/5
-ypredDFFinalDetails["Target"] =  (ytest_RBF + ytest_KNNpred + ytest_XGS)/3  
+ypredDFFinalDetails["Target"] =  (ytest_RF + ytest_KNNpred + ytest_XGS)/3  
 
 ypredDFFinalDetails.to_csv(test_result_file2, index=False)
 
 sumbission1 = ypredDFFinalDetails[["ID"]]
-sumbission1["target"] = ypredDFFinalDetails["Target"] 
+sumbission1["target"] = (ytest_RF + ytest_KNNpred + ytest_XGS)/3  
 sumbission1.to_csv(test_result_file21, index=False)
 
 sumbission2 = ypredDFFinalDetails[["ID"]]
